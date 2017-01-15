9aace45a9c5c5a28d14addee8dfa98050dc83264
bob <bob.kinsloe@gmail.com>
2017-01-15 15:07:28 -0600

    installed laravel; boilerplat api controllers

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+  /**
+   * login endpoint.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function login()
+  {
+      //
+  }
+
+  /**
+   * logout endpoint.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function logout()
+  {
+      //
+  }
+}
